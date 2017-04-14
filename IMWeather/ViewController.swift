a8d293cf914d0b63cb16d6a15abfc1b5e42517bd
imwallet <lsh249410504@163.com>
2017-04-14 10:45:51 +0800

    Initial Commit

---
+++ IMWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IMWeather
+//
+//  Created by imwallet on 17/4/14.
+//  Copyright © 2017年 imWallet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
